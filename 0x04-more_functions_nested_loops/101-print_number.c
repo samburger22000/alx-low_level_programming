3219762eb76397018233c456a13cd43f6165e408
samburger22000 <samburger22000@gmail.com>
2023-12-21 20:27:01 -0800

    Optimization of code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,18 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_number - prints an int numbers.
- * @n: number tested
- * Return: Nothing.
+ * print_number - Prints an interger.
+ * @n: The integer to prints.
+ * Return: Nothing!
  */
 void print_number(int n)
 {
 	unsigned int k = n;
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
-		putchar( '-');
+	n *= -1;
+	k = n;
+	_putchar('-');
 	}
 	k /= 10;
 	if (k != 0)
